b48354b1e8a8868c059bcf428997272654fc4b1e
hduc2004dl <hduc2003tt@gmail.com>
2025-07-30 08:56:44 +0700

    Add files via upload

---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] private Transform pointA;
+    [SerializeField] private Transform pointB;
+    [SerializeField] private float speed = 2f;
+
+    private Vector3 target;
+    private Vector3 lastPosition;
+
+    void Start()
+    {
+        target = pointA.position;
+        lastPosition = transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, target) < 0.1f)
+        {
+            target = (target == pointA.position) ? pointB.position : pointA.position;
+        }
+
+        lastPosition = transform.position;
+    }
+
+    void LateUpdate()
+    {
+        lastPosition = transform.position;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Vector3 platformDelta = transform.position - lastPosition;
+            collision.transform.position += platformDelta;
+        }
+    }
+}
